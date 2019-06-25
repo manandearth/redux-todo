1dee79dc9a4f768471c051489a14a16fa8304403
manandearth <adamgefen@posteo.net>
2019-06-25 09:16:10 +0200

    Update `TodoApp` with props for spawning lists

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,23 +5,35 @@ import VisibilityFilters from './components/VisibilityFilters.js';
 import TodoComp from './components/TodoComp.js';
 import AddList from './components/AddList.js';
 import './App.css';
-
+import { connect } from 'react-redux';
 
 class TodoApp extends React.Component {
-   
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
 			<div className="todo-app">
 			  <h1>Todo List</h1>
               <h2>Extending the redux basic example. </h2>
-              <AddTodo />
-              <TodoList />
-              <VisibilityFilters />
-              <AddList />
-              <TodoComp /> {/*the new element */}
+               <AddList />
+              <div>{this.props.lists && this.props.lists !== 0 ?
+                  this.props.lists.map(list =>
+                      <div>
+                     <TodoComp />
+                   </div>
+                  ) : this.props.a}</div>
             </div>
 	);
     }
 }
 
-export default TodoApp;
+function mapStateToProps(state) {
+    return {
+        lists: Object.keys(state.todos.lists),
+        a: "Bob"
+    };
+}
+
+export default connect(mapStateToProps)(TodoApp);
