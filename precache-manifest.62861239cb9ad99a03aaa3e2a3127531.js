6c3919b5e7fd1efbb0da60f1127ecce69e01e6c5
manandearth <adamgefen@posteo.net>
2019-06-26 12:21:56 +0200

    Updates

---
+++ precache-manifest.62861239cb9ad99a03aaa3e2a3127531.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa41c5a5dce88ac449c8400c66dec17",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "418fb8bb36a2f4bf68b1",
+    "url": "/redux-todo/static/css/main.6321caa5.chunk.css"
+  },
+  {
+    "revision": "b53982ccd06f10081c31",
+    "url": "/redux-todo/static/js/2.6aecdcd7.chunk.js"
+  },
+  {
+    "revision": "418fb8bb36a2f4bf68b1",
+    "url": "/redux-todo/static/js/main.25560af7.chunk.js"
+  },
+  {
+    "revision": "4a2d5658e1b8a4be0a5f",
+    "url": "/redux-todo/static/js/runtime~main.134d15df.js"
+  }
+]);
