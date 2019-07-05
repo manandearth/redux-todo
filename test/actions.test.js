76c467e3ac47762c0020a6086fa1360767d5cbe8
manandearth <adamgefen@posteo.net>
2019-07-05 20:57:58 +0200

    Add action tests

--- test/actions.test.js
+++ test/actions.test.js
@@ -1,6 +1,8 @@
-import * as actions from '../src/redux/actions.js';
-import * as types from '../src/redux/actionTypes.js';
+import * as actions from '../src/redux/actions';
+import * as types from '../src/redux/actionTypes';
+import reducer from '../src/redux/reducers/todos';
 
+// testing actions
 
 describe('actions', () => {
   it('should create an action to add a todo', () => {
@@ -45,4 +47,50 @@ describe('actions', () => {
 
 test('the best flavour is grapefruit', () => {
 	expect(2 + 3).toBe(5);
+});
+
+
+// testing reducers
+const initialState = {
+	lists: {1: "Todo"},
+  allIds: [],
+  byIds: {}
+}; 
+
+
+describe('todos reducer', () => {
+	it('should return the initial state', () => {
+		expect(reducer(undefined, {})).toEqual(
+			initialState
+		);
+	});
+	it('renames the list ', () => {
+		expect(reducer(initialState, {
+			type: types.RENAME_LIST,
+			payload: {id: 1, title: "spanking new name"}
+		})).toEqual(
+			{
+				lists: {1: "spanking new name"},
+					allIds: [],
+					byIds: {}
+			}
+		
+		);
+	});
+	//TODO the state seem to follow from one to the next even though I use `initialState again and it is a const
+	it('adds a list', () => {
+		expect(reducer(initialState, {
+			type: types.ADD_LIST,
+			payload: {
+				listId: 2,
+				list: 'another one bites the dust'}})).toEqual(
+					{
+						lists: {
+							1: "spanking new name",
+							2: "another one bites the dust"
+						},
+						allIds: [],
+						byIds: {}
+					});
+	});
 });
