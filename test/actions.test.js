d5aa144526200ce9b9b6676d27bdc7ccdf979b99
manandearth <adamgefen@posteo.net>
2019-07-05 12:11:54 +0200

    Add doc.org and first test passes

---
+++ test/actions.test.js
@@ -0,0 +1,28 @@
+import * as actions from '../src/redux/actions.js';
+import * as types from '../src/redux/actionTypes.js';
+
+
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+      const text = 'Finish docs';
+    const expectedAction = {
+      type: types.ADD_TODO,
+		payload: {
+			content: undefined,
+			id: 1,
+			list: text
+			
+		}};
+      expect(actions.addTodo(text)).toEqual(expectedAction);
+  });
+});
+
+
+
+
+
+// const Todo = require('../../src/components/Todo');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
--- test/components/Todo.test.js
+++
@@ -1,5 +0,0 @@
-const Todo = require('../../src/components/Todo');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
