f516ab3a05623b96ff4cde5782dfc462109473c0
manandearth <adamgefen@posteo.net>
2019-07-06 17:59:32 +0200

    Make TodoComp and its div container fill their Gridblock

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -19,52 +19,61 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 class TodoApp extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { rowHeight: 10 };
+        this.handleResize = this.handleResize.bind(this);
     }
 
     handleGetListById(list) {
         return getListById(list);
     }
+
+    handleResize() {
+        const newState = {rowHeight: 10};
+        this.setState(() => newState);
+    }
     
     render() {
 
-var layout = [
-    {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-    {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-    {i: 'c', x: 4, y: 0, w: 1, h: 2}];
         return (
             <div>
               <Navbar />
-			  <div className="todo-app">
-                <div className="app-header">
-			      <p>Create multiple lists.<br /> 
-                    Click an item to toggle completion.<br />
-                    Filter the views: <b>'all/completed/incomplete'</b>.<br />
-                    Add a new list using the field below.<br />
-                    Click a list title to rename.
-                  </p>
-                </div>
-                <AddList />
+              <div className="todo-app">
                 <ResponsiveGridLayout className="layout"
-                                      layout={layout}
-            /* cols={3} */
-                          breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-                                      cols={{lg: 5, md: 4, sm: 3, xs: 2, xxs: 1}}
-                                      width={1200}
-                            draggableCancel="input,textarea" /* Cancel dragging on the input fields to allow focus and typing  */
-                >{this.props.listsArray && this.props.listsArray !== 0 ?
-                    this.props.listsArray.map(list =>
-                   <div key={"TodoComp" + list}>       
+                                      cols={4} rowHeight={this.state.rowHeight}
+                                      breakpoints={{four: 1600, three: 1200, two: 800, one: 400}} 
+                                      cols={{four: 4, three: 3, two: 2, one: 1}}
+                                      verticalCompact={true}
+                                      compactType={'vertical'}
+                                      draggableCancel="input,textarea" /* Cancel dragging on the input fields to allow focus and typing  */
+                                      /* onResize={this.handleResize} */
+                >
+                
+                  
+                  <div className="app-header"
+                       key="app-header"
+                       data-grid={{x: 0, y: 0, w: 0.97, h: 10,  static: true}}>{/*for `smoother` steps in resizing `h` takes 10 row heights (10 px each)   */}
+			        <p>Click an item to toggle completion.<br />
+                      Filter the views: <b>'all/completed/incomplete'</b>.<br />
+                      Click a list title to rename.
+                    </p>
+                    <AddList /> 
+                  </div>
+                  
+                  
+                  {this.props.listsArray && this.props.listsArray !== 0 ?
+                      this.props.listsArray.map(list =>
+                          <div className="todo-comp-container"
+                            key={"TodoComp" + list}
+                                data-grid={{x: 1, y: 2, w: 1, h: 9}}>
                             <TodoComp
-                              
                               listName={this.props.lists[list]}
                               list={list}
                             />
-                            {console.log(this.props.lists[list])}
-                          </div>
-                    ) : this.props.a}
+                   </div> ) : this.props.a}
               </ResponsiveGridLayout>
               </div>
-            </div>
+                  </div>
+            
 	);
     }
 }
@@ -72,8 +81,7 @@ var layout = [
 function mapStateToProps(state) {
     return {
         listsArray: Object.keys(state.todos.lists),
-        lists: state.todos.lists,
-        a: "Bob"
+        lists: state.todos.lists
     };
 } 
 
